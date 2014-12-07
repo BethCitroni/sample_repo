416346e51757ce9970331f239cbeafca8e197e28
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2014-12-07 16:54:39 -0500

    updating ruby test

--- .linter/run_tests.rb
+++ .linter/run_tests.rb
@@ -1,21 +1,27 @@
+#!/usr/bin/env ruby
 require "English"
 
 # Get diffed files
 git_diff = `git diff --name-only HEAD~1`
 
 # Ignore deleted or renamed
-edited_files = git_diff.each_line.map(&:chomp).select! { |file| if File.exists?(file) }
+edited_files = git_diff.each_line.map(&:chomp).select! { |file| File.exists?(file) } || []
 
 
 edited_files.map! do |file|
   case File.extname(file)
   when ".rb"
-    `rubocop ${file}`
+    `rubocop ${file} --config .linter/.rubocop.yml`
   when ".js"
-    `jshint ${file}`
+    `jshint ${file} --config=.linter/.jshint`
+  when ".css"
+    `csscomb ${file} -c .linter/.csscomb.json -lv`
+  when ".html"
+    `htmllint ${file} --rc .linter/.htmllintrc`
   else
     "ok"
   end
   $CHILD_STATUS.success?
 end
 
+exit(1) if edited_files.include? false
