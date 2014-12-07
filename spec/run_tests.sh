69f6b7a534885af4a86b1f512ffada4540dcc32c
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2014-12-07 14:43:33 -0500

    removes exit codes from script

--- spec/run_tests.sh
+++ spec/run_tests.sh
@@ -2,9 +2,12 @@
 for file in $(git diff --name-only HEAD~1); do
   if [ -a $file ]; then
     case ${file: -3} in
-      ".rb") rubocop $file || exit 1;;
+      # ".rb") rubocop $file || exit 1;;
+      # ".js") jshint $file  || exit 1;;
 
-      ".js") jshint $file  || exit 1;;
+      ".rb") rubocop $file;;
+      ".js") jshint  $file;;
+
 
       *) echo "ok";
     esac
