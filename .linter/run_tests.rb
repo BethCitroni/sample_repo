c0da5920e97ebcaf3815baea7b4ebbc5f82a0a72
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2014-12-07 17:32:51 -0500

    works on ruby tests

--- .linter/run_tests.rb
+++ .linter/run_tests.rb
@@ -7,20 +7,27 @@ git_diffs = `git diff --name-only HEAD~1`.each_line.map(&:chomp)
 # Ignore deleted or renamed
 edited_files = git_diffs.select { |file| File.exist?(file) } || []
 
-edited_files.map! do |file|
-  case File.extname(file)
-  when '.rb'
-    `rubocop ${file} --config .linter/.rubocop.yml`
-  when '.js'
-    `jshint ${file} --config=.linter/.jshint`
-  when '.css'
-    `csscomb ${file} -c .linter/.csscomb.json -lv`
-  when '.html'
-    `htmllint ${file} --rc .linter/.htmllintrc`
-  else
-    'ok'
-  end
-  $CHILD_STATUS.success?
+results = []
+
+edited_files.each do |file|
+  msg = case File.extname(file)
+        when '.rb'
+          `rubocop ${file} --config .linter/.rubocop.yml`
+          "Linting Ruby file #{file}"
+        when '.js'
+          `jshint ${file} --config=.linter/.jshint`
+          "Linting JS file #{file}"
+        when '.css'
+          `csscomb ${file} -c .linter/.csscomb.json -lv`
+          "Linting css file #{file}"
+        when '.html'
+          `htmllint ${file} --rc .linter/.htmllintrc`
+          "Linting html file #{file}"
+        else
+          'ok'
+        end
+  results.push( $CHILD_STATUS.success? )
+  $stdout.puts(msg)
 end
 
-exit(1) if edited_files.include? false
+exit(1) if results.include? false
