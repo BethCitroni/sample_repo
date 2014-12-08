6474e658f59e390676224665f024b573582b0441
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2014-12-08 01:26:42 -0500

    updates with secure api

---
+++ .linter/ex_boyfriends.rb
@@ -0,0 +1,30 @@
+EX_BOYFRIENDS = [{
+  :name   => "Brandon Borello",
+  :inspired => ["Tim McGraw"]
+},
+{
+  :name     => "Drew",
+  :inspired => ["Teardrops on My Guitar"]
+},
+{
+  :name     => "Sam Armstrong",
+  :inspired => ["Should've Said No"]
+},
+{
+  :name     => "Joe Jonas",
+  :inspired => ["Forever & Always", "Last Kiss", "Better than Revenge"]
+},
+{
+  :name     => "Taylor Lautner",
+  :inspired => ["Back to December"]
+},
+{
+  :name     => "Jake Gyllenhaal",
+  :inspired => ["We Are Never Ever Getting Back Together", "State of Grace",
+    "All Too Well", "Girl at Home", "The Moment I Knew"]
+},
+{
+  :name     => "John Mayer",
+  :inspired => ["Dear John", "I Knew You Were Trouble"]
+}]
+
