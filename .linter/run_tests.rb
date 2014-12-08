2ebdb656771d536e8e771c4fd1b5d7d29055f0c5
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2014-12-07 21:48:22 -0500

    updates ruby file

--- .linter/run_tests.rb
+++ .linter/run_tests.rb
@@ -7,30 +7,34 @@ git_diffs = `git diff --name-only HEAD~1`.each_line.map(&:chomp)
 # Ignore deleted or renamed
 edited_files = git_diffs.select { |file| File.exist?(file) } || []
 
-results = edited_files.map do |file|
-  msg = case File.extname(file)
-        when '.rb'
-          `rubocop ${file} --config .linter/.rubocop.yml`
-          "Linting Ruby file #{file}"
-        when '.js'
-          `jshint ${file} --config=.linter/.jshint`
-          "Linting JS file #{file}"
-        when '.css'
-          `csscomb ${file} -c .linter/.csscomb.json -lv`
-          "Linting css file #{file}"
-        when '.html'
-          `htmllint ${file} --rc .linter/.htmllintrc`
-          "Linting html file #{file}"
-        else
-          'ok'
-        end
-  $stdout.puts(msg)
-  $CHILD_STATUS.success?
+edited_files.map! do |file|
+
+  name = File.basename(file)
+
+  message = case File.extname(file)
+            when '.rb'
+              `rubocop ${file} --config .linter/.rubocop.yml`
+            when '.js'
+              `jshint ${file} --config=.linter/.jshint`
+            when '.css'
+              `csscomb ${file} -c .linter/.csscomb.json -lv`
+            when '.html'
+              `htmllint ${file} --rc .linter/.htmllintrc`
+            else
+              'ok'
+            end
+
+  success = $CHILD_STATUS.success?
+
+  $stdout.puts(message) if !success
+
+  { name: name, message: message, success: success }
 end
 
-if results.include? false
-  $stdout.puts("#{results.count(&:!)} error(s)")
+if edited_files.select { |result| !result[:success] }.any?
+  $stdout.puts("Errors: #{edited_files.count { |result| !result[:success] }}")
   exit 1
+else
+  $stdout.puts("No errors")
+  exit 0
 end
-
-
